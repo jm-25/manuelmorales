be8f22dea000e5483e821e73155ec6f904452eec
jorgemejia25 <jor.mejiag@gmail.com>
2018-11-20 16:04:29 -0600

    20181120

---
+++ wp-content/themes/equilibrium/styles/custom.php
@@ -0,0 +1,4 @@
+/*	Custom Styles
+	....................................................................................................................................... */
+
+	html body { background: #ffffff url('http://manuelmorales.mx/wp-content/themes/equilibrium/images/layout/scan-lines.png') repeat left top; }
