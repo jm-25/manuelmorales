d9006360e700f710a5259f3078c3a4488a36c3f9
jm-25 <jor.mejiag@gmail.com>
2024-12-03 13:20:17 -0600

    SSL

--- wp-content/themes/equilibrium/styles/custom.php
+++ wp-content/themes/equilibrium/styles/custom.php
@@ -1,4 +1,4 @@
 /*	Custom Styles
 	....................................................................................................................................... */
 
-	html body { background: #ffffff url('http://manuelmorales.mx/wp-content/themes/equilibrium/images/layout/scan-lines.png') repeat left top; }
+	html body { background: #ffffff url('https://manuelmorales.mx/wp-content/themes/equilibrium/images/layout/scan-lines.png') repeat left top; }
